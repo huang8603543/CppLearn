8c93c86bb7ac0194cd7ac2c706e9c19561210e34
黄智超 <519795619@qq.com>
2018-03-28 08:43:33 +0800

    Add

---
+++ HappyTest/Assets/Scripts/ResMgr/AssetCache.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Happy.Res
+{
+    public abstract class AssetCache
+    {
+        private int _refCount = 0;
+
+        public int RefCount
+        {
+            get
+            {
+                return _refCount;
+            }
+        }
+
+        private HashSet<int> objSet;
+        public LinkedListNode<AssetCache> linkListNode;
+
+        /// <summary>
+        /// 最后使用时间///
+        /// </summary>
+        public float LastUsedTime
+        {
+            get;
+            set;
+        }
+
+        public int AddRefCount(int refCount = 1)
+        {
+            _refCount += refCount;
+            return _refCount;
+        }
+
+        public int DecRefCount(int refCount = 1)
+        {
+            if (_refCount <= 0)
+            {
+                _refCount = 0;
+                return _refCount;
+            }
+
+            _refCount -= refCount;
+            if (_refCount < 0)
+                _refCount = 0;
+            return _refCount;
+        }
+    }
+}
